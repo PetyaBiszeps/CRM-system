80a75abd22d507a468684251c2c55ec2caaedbeb
YP00 <personalYP00@icloud.com>
2026-01-13 19:29:20 +0200

    Sidebar menu structure init.

--- app/components/layout/Sidebar.vue
+++ app/components/layout/Sidebar.vue
@@ -1,15 +1,35 @@
+<script setup lang="ts">
+interface sidebarTab {
+  name: string
+  icon: string
+  link: string
+}
+
+const sidebarTab: sidebarTab[] = [{
+  name: 'Dashboard',
+  icon: 'material-symbols:interactive-space-outline',
+  link: '/',
+}, {
+  name: 'Goods',
+  icon: 'material-symbols:shopping-bag-speed-outline',
+  link: '/goods',
+}]
+</script>
+
 <template>
   <aside :class="['sidebar']">
     <h1>CRM</h1>
 
     <ul>
-      <li>
-        <Icon
-          name="material-symbols:interactive-space-outline"
-          size="24"
-        />
+      <li
+        v-for="tab in sidebarTab"
+        :key="tab.link"
+      >
+        <NuxtLink :to="tab.link">
+          <Icon :name="tab.icon" />
 
-        <p>Dashboard</p>
+          <p>{{ tab.name }}</p>
+        </NuxtLink>
       </li>
     </ul>
   </aside>
