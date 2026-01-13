4f02d172a43b5bfe10e0b94562d39facc3fe8e38
YP00 <personalYP00@icloud.com>
2026-01-13 03:06:18 +0200

    nuxt color-mode module added, root and variables scss files created.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,19 @@
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@pinia/nuxt', '@nuxt/fonts'],
+  modules: [
+    '@nuxt/eslint',
+    '@pinia/nuxt',
+    '@nuxt/fonts',
+    '@nuxtjs/color-mode',
+  ],
   devtools: {
     enabled: true,
   },
   css: ['~/assets/styles/main.scss'],
+  colorMode: {
+    preference: 'system',
+    fallback: 'light',
+    classSuffix: '',
+  },
   compatibilityDate: '2025-07-15',
   eslint: {
     config: {
