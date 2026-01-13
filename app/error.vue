99437bce5ee4d0318f58ff9e6348c726344e4658
YP00 <personalYP00@icloud.com>
2026-01-13 22:52:58 +0200

    error page init.

---
+++ app/error.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type {
+  NuxtError,
+} from '#app'
+
+const { error } = defineProps<{
+  error: NuxtError
+}>()
+</script>
+
+<template>
+  <NuxtLayout name="error">
+    <h1>{{ error.statusCode }}</h1>
+    <h3>{{ error.message }}</h3>
+
+    <NuxtLink to="/">Go back home</NuxtLink>
+  </NuxtLayout>
+</template>
---
+++ app/layouts/error.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="wrapper">
+    <div class="content">
+      <div class="error">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
