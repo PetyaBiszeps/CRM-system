532ca8e0d1ec66b8fdec3907c2f24c3767576a9d
YP00 <personalYP00@icloud.com>
2026-01-13 16:59:14 +0200

    ESLint prop sorting added.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,22 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt()
+export default withNuxt({
+  rules: {
+    'vue/attributes-order': ['error', {
+      order: [
+        'CONDITIONALS', // v-if, v-else
+        'LIST_RENDERING', // v-for
+        'TWO_WAY_BINDING', // v-model
+        'OTHER_DIRECTIVES', // v-click-outside
+        'RENDER_MODIFIERS', // v-once
+        'CONTENT', // v-html, v-text
+        'DEFINITION', // :is
+        'UNIQUE', // :ref, :key
+        'GLOBAL', // :id
+        'OTHER_ATTR', // :name, :type, :placeholder, :disabled, :readonly, :autocomplete, :model-value, :class, :style
+        'EVENTS', // @click, @focus
+      ],
+    }],
+  },
+})
