703d77a499a87dc5e76cbcef55b0281abdc32401
YP00 <personalYP00@icloud.com>
2026-01-16 03:11:31 +0200

    remove global loader, add basic skeleton.

--- app/app.vue
+++ app/app.vue
@@ -1,24 +1,5 @@
-<script setup lang="ts">
-const loader = useLoader()
-const {
-  isLoading
-} = useLoadingIndicator()
-</script>
-
 <template>
-  <div>
-    <Transition name="fade">
-      <AppLoader
-        v-if="loader || isLoading"
-        :overlay="true"
-        :center="true"
-        :filled="true"
-        :size="400"
-      />
-    </Transition>
-
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ app/components/ui/skeleton/Skeleton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  width?: string | number
+  height?: string | number
+}>()
+</script>
+
+<template>
+  <div
+    :class="['skeletonWrapper']"
+    :style="[{
+      width: typeof width === 'number' ? `${width}px` : width,
+      height: typeof height === 'number' ? `${height}px` : height,
+      borderRadius: '8px',
+    }]"
+  />
+</template>
